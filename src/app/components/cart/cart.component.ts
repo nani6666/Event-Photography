697bdc19c962b0701a2e3bb36015e6966d65184c
nani6666 <raviteja.m6666@gmail.com>
2018-05-24 10:38:24 +0530

    code refactored

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -9,11 +9,25 @@ export class CartComponent implements OnInit {
   public counter: any;
   itemPrice: any;
   checkoutDiv: boolean;
-  constructor(private router: Router) { }
+  favoritesDiv: boolean;
+  cartDiv: boolean;
+  productval: any;
+  totalval: any;
+  itemsArray: any;
+  checkoutdisabled: boolean;
+  constructor(private router: Router) {
+    this.itemsArray = [];
+    this.totalval = 0;
+
+  }
   ngOnInit() {
+    // this.favoritesDiv = true;
+    this.productval = '';
     this.checkoutDiv = false;
+    this.cartDiv =  true;
     this.counter = 0 ;
     this.itemPrice = 100;
+    this.checkoutbtn();
   }
 
   increment() {
@@ -28,7 +42,58 @@ export class CartComponent implements OnInit {
     }
   }
   checkout() {
+    this.favoritesDiv = false;
     this.checkoutDiv = true;
+    this.cartDiv =  false;
+  }
+
+  gotocart() {
+    this.favoritesDiv = false;
+    this.checkoutDiv = false;
+    this.cartDiv =  true;
+  }
+  gotofavourtes() {
+    this.favoritesDiv = true;
+    this.checkoutDiv = false;
+    this.cartDiv =  false;
+  }
+  getItems(val) {
+   console.log(val);
+  if (val == '4 × 5') {
+    this.productval = 150;
+    this.itemsArray.push({'name': '4 × 5' , 'price': 150});
+   } else if (val == '6 × 7') {
+    this.productval = 250;
+    this.itemsArray.push({'name': '6 × 7' , 'price': 250});
+   } else if (val == '8 × 9') {
+    this.productval = 350;
+    this.itemsArray.push({'name': '8 × 9' , 'price': 350});
+   } else if (val == '25 × 60') {
+    this.productval = 850;
+    this.itemsArray.push({'name': '25 × 60' , 'price': 850});
+  }
+
+  this.totalval += this.productval ;
+   // console.log(this.totalval);
+   this.checkoutbtn();
+  }
+
+  deleteprod(val , ind) {
+   console.log(val , ind);
+   this.itemsArray.splice(ind , 1);
+   if (this.totalval >= 150) {
+    this.totalval -= val.price;
+  } else {
+    this.totalval = 0;
+  }
+  }
+
+  checkoutbtn() {
+    if (this.totalval === 0) {
+      this.checkoutdisabled = true;
+     } else {
+       this.checkoutdisabled = false;
+     }
   }
 
 }
